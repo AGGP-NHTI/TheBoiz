922c483ea04cb75820ad7ca4df23fd16017f3071
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-04-14 18:11:42 -0400

    changes

---
+++ Assets/Scripts/MobSpawnSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawnSystem : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject Zombie;
+    public GameObject Cultist;
+    public GameObject Bleemeay;
+    int Selection;
+    public Collider2D collider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Selection = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Selection == 0)
+       {
+            SpawnZombie();
+       }
+       if(Selection == 1)
+       {
+            SpawnCultist();
+       }
+       if(Selection == 2)
+       {
+            SpawnBleemeay();
+       }
+    }
+
+    public void SpawnBleemeay()
+    {
+        Instantiate(Bleemeay, transform.position, Quaternion.identity);
+    }
+    public void SpawnCultist()
+    {
+        Instantiate(Cultist, transform.position, Quaternion.identity);
+    }
+    public void SpawnZombie()
+    {
+        Instantiate(Zombie, transform.position, Quaternion.identity);
+    }
+}
+
