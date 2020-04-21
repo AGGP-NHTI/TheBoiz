1c417a8e737fc0a08518dc786cdc17256556de7b
TrainsZelda <62075803+TrainsZelda@users.noreply.github.com>
2020-04-21 12:33:29 -0800

    jbjfbff

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,15 @@
+﻿public abstract class State
+{
+    protected PlayerStateMachine stateMachine;
+
+    public abstract void Tick();
+
+    public virtual void OnStateEnter() { }
+    public virtual void OnStateExit() { }
+
+    public State(PlayerStateMachine sm)
+    {
+        this.stateMachine = sm;
+    }
+
+}
