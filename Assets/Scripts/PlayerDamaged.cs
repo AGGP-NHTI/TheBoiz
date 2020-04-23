8ab04e366e07bd2f17c2479ff4144933a37f2df3
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-04-23 18:40:35 -0400

    New Damaged state
    Created a new Damaged state for the PlayerStateMachine. Simply changes the sprite's color for the default to red, then back again, before returning to the default state.

--- Assets/Scripts/PlayerDamaged.cs
+++ Assets/Scripts/PlayerDamaged.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDamaged : MonoBehaviour
+public class PlayerDamaged : State
 {
-    // Start is called before the first frame update
-    void Start()
+    private Color damagedColor, normalColor;
+
+    public PlayerDamaged(PlayerStateMachine sm) : base(sm)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStateEnter()
     {
-        
+        normalColor = stateMachine.sprite_renderer.color;
+        damagedColor = new Color(1.0f, 0.24f, 0.24f); // Red
+    }
+
+    public override void Tick()
+    {
+        Color currentColor = Color.Lerp(normalColor, damagedColor, 0.2f);
+        stateMachine.sprite_renderer.color = currentColor;
+
+        // Now go back
+        currentColor = Color.Lerp(damagedColor, normalColor, 0.2f);
+        stateMachine.sprite_renderer.color = currentColor;
+
+        stateMachine.SetState(null);
     }
 }
