a38847bac700fccb3b30de82b4ab3536362d6c42
TrainsZelda <62075803+TrainsZelda@users.noreply.github.com>
2020-04-28 15:14:52 -0800

    hi

---
+++ Assets/Scripts/GameOverScripts/Branch.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    public TextAsset txt;
+
+    public int startL;
+    public int endDaL;
+
+    public TXTscript txtBox;
+
+    public bool destroy;
+
+    public bool buttonPress;
+    public bool waitForButton;
+
+    //public GameObject box;
+    public GameObject world;
+    public GameObject npc;
+   
+    public GameObject Byes;
+    public GameObject Bno;
+    public GameObject BHeRight;
+    public GameObject BHeLie;
+    public GameObject Bdoom;
+    public GameObject Blife;
+    public GameObject Bevil;
+    public GameObject Bhero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtBox = FindObjectOfType<TXTscript>();
+
+        Byes.SetActive(false);
+        Bno.SetActive(false);
+        BHeLie.SetActive(false);
+        BHeRight.SetActive(false);
+        Bdoom.SetActive(false);
+        Blife.SetActive(false);
+        Bhero.SetActive(false);
+        Bevil.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForButton && Input.GetKeyDown(KeyCode.Return))
+        {
+            txtBox.reload(txt);
+            txtBox.curL = startL;
+            txtBox.endL = endDaL;
+            txtBox.enableTXTbox();
+
+            if (txtBox.count == 0)
+            {
+                txtBox.isBranch = true;
+                Byes.SetActive(true);
+                Bno.SetActive(true);
+            }
+
+            if (destroy)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            if (buttonPress)
+            {
+                waitForButton = true;
+                return;
+            }
+
+            txtBox.reload(txt);
+            txtBox.curL = startL;
+            txtBox.endL = endDaL;
+            txtBox.enableTXTbox();
+
+            if (destroy)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            waitForButton = false;
+        }
+    }
+
+    public void yes()
+    {
+        startL = 19;
+        endDaL = 19;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+    }
+
+    public void no()
+    {
+        startL = 16;
+        endDaL = 16;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+    }
+
+    public void next()
+    {
+        startL = 21;
+        endDaL = 23;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+    }
+
+    public void lies()
+    {
+        startL = 26;
+        endDaL = 28;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+
+        txtBox.count = 10;
+    }
+
+    public void right()
+    {
+        startL = 31;
+        endDaL = 33;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+    }
+
+    public void doom()
+    {
+        startL = 54;
+        endDaL = 54;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+
+        txtBox.isBranch = false;
+
+        Destroy(world);
+        Destroy(gameObject);
+        Destroy(npc);
+    }
+
+    public void life()
+    {
+        startL = 36;
+        endDaL = 37;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+    }
+
+    public void hero()
+    {
+        startL = 47;
+        endDaL = 51;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+
+        txtBox.count = 10;
+    }
+
+    public void evil()
+    {
+        startL = 40;
+        endDaL = 44;
+        txtBox.reload(txt);
+        txtBox.curL = startL;
+        txtBox.endL = endDaL;
+        txtBox.enableTXTbox();
+
+        
+    }
+    public void killed()
+    {
+        Destroy(world);
+        Destroy(npc);
+    }
+
+    public void choice2()
+    {
+        BHeLie.SetActive(true);
+        BHeRight.SetActive(true);
+    }
+
+    public void choice3()
+    {
+        Bdoom.SetActive(true);
+        Blife.SetActive(true);
+    }
+
+    public void choice4()
+    {
+        Bhero.SetActive(true);
+        Bevil.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GameOverScripts/TXTscript.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TXTscript : MonoBehaviour
+{
+    public GameObject txtBox;
+    public Text txt;
+    public int curL;
+    public int endL;
+
+    public TextAsset txtFile;
+    public string[] txtLines;
+
+    public bool isActive;
+    public bool stopMov;
+
+    public movement Pmov;
+
+    public Branch scr;
+
+    private bool isType = false;
+    private bool cancType;
+    public float typeSpeed;
+
+    public bool isBranch = false;
+    public int count = 0;
+
+    void Start()
+    {
+        Pmov = FindObjectOfType<movement>();
+
+        scr = FindObjectOfType<Branch>();
+
+        if (txtFile != null)
+        {
+            txtLines = (txtFile.text.Split('\n'));
+        }
+
+        if (endL == 0)
+        {
+            endL = txtLines.Length - 1;
+        }
+
+        if (isActive)
+        {
+            enableTXTbox();
+        }
+        else
+        {
+            disableTXTbox();
+        }
+    }
+
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+        //txt.text = txtLines[curL];
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isType)
+            {
+                curL += 1;
+
+                if (curL > endL)
+                {
+                    disableTXTbox();
+                }
+                else
+                {
+                    StartCoroutine(txtScroll(txtLines[curL]));
+                }
+            }
+            else if (isType && !cancType)
+            {
+                cancType = true;
+               
+            }
+        } 
+    }
+
+    private IEnumerator txtScroll(string lineOtxt)
+    {
+        int letter = 0;
+        txt.text = "";
+        isType = true;
+
+        while(isType && !cancType && (letter < lineOtxt.Length - 1))
+        {
+            txt.text += lineOtxt[letter];
+            letter += 1;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        txt.text = lineOtxt;
+        isType = false;
+        cancType = false;
+    }
+
+    public void enableTXTbox()
+    {
+        txtBox.SetActive(true);
+        isActive = true;
+
+        if (stopMov)
+        {
+            Pmov.canMov = false;
+        }
+
+        StartCoroutine(txtScroll(txtLines[curL]));
+    }
+
+    public void disableTXTbox()
+    {
+        if (isBranch)
+        {
+            count += 1;
+            scr.next();
+            
+            if (count == 2)
+            {
+                txtBox.SetActive(false);
+                scr.choice2();
+            }
+            if (count == 3)
+            {
+                txtBox.SetActive(false);
+                scr.choice3();
+            }
+            if (count == 4)
+            {
+                txtBox.SetActive(false);
+                scr.choice4();
+            }
+            if(count == 5)
+            {
+                txtBox.SetActive(false);
+                scr.killed();
+            }
+            if (count > 10)
+            {
+                txtBox.SetActive(false);
+            }
+        }
+        if(!isBranch)
+        {
+            txtBox.SetActive(false);
+            isActive = false;
+            Pmov.canMov = true;
+        }
+    }
+
+    public void reload(TextAsset txt)
+    {
+        if (txt != null)
+        {
+            txtLines = new string[1];
+            txtLines = (txt.text.Split('\n'));
+        }
+    }
+}
