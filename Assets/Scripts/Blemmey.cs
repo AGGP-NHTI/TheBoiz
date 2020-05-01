bc4ba7f5d5e5015816c7059ee2f41040f3292807
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-04-30 22:28:48 -0400

    temp blemmey

---
+++ Assets/Scripts/Blemmey.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blemmey
+{
+
+    public float speed;
+    public float damage;
+    public float attack_cooldown;
+
+    public Blemmey()
+    {
+        speed = 4f;
+        damage = 40f;
+        attack_cooldown = 2f;
+    }
+}
---
+++ Assets/Scripts/BlemmeyStateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlemmeyStateMachine : MonoBehaviour
+{
+    public float health;
+    public bool isAlive;
+
+    public float lastDamageAmt; // Value of the last amount of damage taken -- so we can pass in proper weapon damage
+
+    public State currentState;
+
+    private void Start()
+    {
+        currentState = null;
+        health = 100;
+        isAlive = true;
+        lastDamageAmt = 0;
+
+        
+    }
+
+    private void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.Tick();
+        }
+
+        if (!isAlive)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetState(State state)
+    {
+        if (state != null)
+        {
+            state.OnStateExit();
+        }
+        currentState = state;
+        //currentStateType = currentState.GetType().Name;
+        if (currentState != null)
+        {
+            currentState.OnStateEnter();
+        }
+    }
+}
