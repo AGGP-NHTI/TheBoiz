67c4ae66964a0c21dfa38f63b7e6b6b415ae6733
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-05-01 20:16:00 -0400

    mob animations

--- Assets/Scripts/ZombieStateMachine.cs
+++ Assets/Scripts/ZombieStateMachine.cs
@@ -12,6 +12,13 @@ public class ZombieStateMachine : MonoBehaviour
     public State currentState;
     public Zombie zom;
 
+    public SpriteRenderer sprite_renderer;
+    public Sprite[] zom_sprites;
+    public int AnimateRunBaseIndex;
+    public int anim_frame = 0;
+
+    public int stepAmt = 0;
+
     private void Start()
     {
         zom = new Zombie();
@@ -19,6 +26,9 @@ public class ZombieStateMachine : MonoBehaviour
         health = 100;
         isAlive = true;
         lastDamageAmt = 0;
+
+        zom_sprites = Resources.LoadAll<Sprite>("Zombie");
+        sprite_renderer = GetComponent<SpriteRenderer>();
 
         SetState(new Attack(this));
     }
@@ -34,6 +44,18 @@ public class ZombieStateMachine : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (step())
+        {
+            anim_frame += 1;
+
+            if (anim_frame > 2)
+            {
+                anim_frame = 0;
+            }
+        }
+
+        animate();
     }
 
     public void SetState(State state)
@@ -48,6 +70,75 @@ public class ZombieStateMachine : MonoBehaviour
         {
             currentState.OnStateEnter();
         }
+    }
+
+    public void animate()
+    {
+        Vector3 pos = transform.position;
+        Vector3 plr_pos = GameObject.Find("player").transform.position;
+        Vector3 dir = (pos - plr_pos).normalized *-1;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        
+
+        if (angle > -25 && angle < 25) //Right
+        {
+            //sprite_renderer.sprite = plr_sprites[20];
+            AnimateRunBaseIndex = 4;
+        }
+        else if (angle > 25 && angle < 75) //Right Up
+        {
+            //sprite_renderer.sprite = plr_sprites[14];
+            AnimateRunBaseIndex = 10;
+        }
+        else if (angle > 75 && angle < 115) //Up
+        {
+            //sprite_renderer.sprite = plr_sprites[2];
+            AnimateRunBaseIndex = 2;
+        }
+        else if (angle > 115 && angle < 165) //Left Up
+        {
+            //sprite_renderer.sprite = plr_sprites[11];
+            AnimateRunBaseIndex = 16;
+        }
+        else if (angle > 165 || angle < -165) //Left
+        {
+            //sprite_renderer.sprite = plr_sprites[5];
+            AnimateRunBaseIndex = 19;
+        }
+        else if (angle > -165 && angle < -115) //Left Down
+        {
+            //sprite_renderer.sprite = plr_sprites[8];
+            AnimateRunBaseIndex = 13;
+        }
+        else if (angle > -115 && angle < -75) //Down
+        {
+            //sprite_renderer.sprite = plr_sprites[0];
+            AnimateRunBaseIndex = 0;
+        }
+        else if (angle > -75 && angle < -25) //Right Down
+        {
+            //sprite_renderer.sprite = plr_sprites[17];
+            AnimateRunBaseIndex = 7;
+        }
+
+        sprite_renderer.sprite = zom_sprites[AnimateRunBaseIndex + anim_frame];
+    }
+
+    bool step()
+    {
+        stepAmt += 1;
+
+
+        if (stepAmt > 30)
+        {
+
+            stepAmt = 0;
+            return true;
+
+        }
+
+        return false;
     }
 
 }
