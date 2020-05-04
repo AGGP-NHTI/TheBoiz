d320eadf66ea6d19cde04b7bd8bdcb160d0dd4c4
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-05-04 18:53:05 -0400

    cultist start

---
+++ Assets/Scripts/CultistStateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistStateMachine : MonoBehaviour
+{
+
+    public float health;
+    public bool isAlive;
+
+    public float lastDamageAmt;
+
+    public State currentState;
+
+    void Start()
+    {
+        currentState = null;
+        health = 100;
+        isAlive = true;
+    }
+
+    
+    void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.Tick();
+        }
+
+        if (!isAlive)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetState(State state)
+    {
+        if (state != null)
+        {
+            state.OnStateExit();
+        }
+        currentState = state;
+        //currentStateType = currentState.GetType().Name;
+        if (currentState != null)
+        {
+            currentState.OnStateEnter();
+        }
+    }
+}
